cd19dac8cb5c1fb372435d3edfcc46ac16050cc6
Nozomi Sugiyama <rioc.sugiyama@gmail.com>
2019-04-22 19:37:32 +0900

    feat(3-5): HelloServiceProviderの作成 P.100-104

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -8,11 +8,6 @@ class HelloController extends Controller
 {
 
     public function index(Request $request) {
-        $data = [
-            ['name'=>'山田たろう', 'mail'=>'taro@yamada'],
-            ['name'=>'田中はなこ', 'mail'=>'hanako@flower'],
-            ['name'=>'鈴木さちこ', 'mail'=>'sachico@happy'],
-        ];
-        return view('hello.index', ['data'=>$data]);
+        return view('hello.index', ['message'=>'Hello!']);
     }
 }
---
+++ src/app/Providers/HelloServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class HelloServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        view()->composer(
+            'hello.index', function ($view) {
+                $view->with('view_message', 'composer message!');
+            }
+        );
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
