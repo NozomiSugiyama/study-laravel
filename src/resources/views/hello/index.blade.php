47990228a86b15a9365698f229483ce675b930b3
Nozomi Sugiyama <rioc.sugiyama@gmail.com>
2019-04-22 19:36:07 +0900

    feat(3-4): @eachによるコレクションビュー P.97

--- src/resources/views/hello/index.blade.php
+++ src/resources/views/hello/index.blade.php
@@ -7,11 +7,10 @@
 @endsection
 
 @section('content')
-    <p>ここが本文のコンテンツです</p>
-    <p>必要なだけ記述できます。</p>
+    <p>ここが本文のコンテンツです。</p>
 
-    @include('components.message', ['msg_title'=>'OK',
-        'msg_content'=>'サブビューです。'])
+    @each('components.item', $data, 'item')
+
 @endsection
 
 @section('footer')
