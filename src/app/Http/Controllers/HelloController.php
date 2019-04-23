346d54d580cdc3b3a536cb6a7aa2a04049df9f0a
Nozomi Sugiyama <rioc.sugiyama@gmail.com>
2019-04-23 15:40:32 +0900

    feat(4-3): Query stringsへのバリデーション P.143

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -17,15 +17,14 @@ class HelloController extends Controller
     public function post(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric|between:0,150',
+            'id' => 'required',
+            'pass' => 'required'
         ]);
         if ($validator->fails()) {
-            return redirect('hello')
-                ->withErrors($validator)
-                ->withInput();
+            $msg = 'クエリーに問題があります。';
+        } else {
+            $msg = 'ID/PASSを受け付けました。フォームを入力下さい。';
         }
-        return view('hello.index', ['msg' => '正しく入力されました！']);
+        return view('hello.index', ['msg' => $msg]);
     }
 }
