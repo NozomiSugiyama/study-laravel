fef3404c4904c2cfaa474f887b405a2290344f57
Nozomi Sugiyama <rioc.sugiyama@gmail.com>
2019-04-24 15:44:03 +0900

    feat(6-1): PersonモデルにgetDataメソッドを追加 P.232
    getDataメソッドでは、名前と年齢をフォーマットしたデータを返す

--- src/app/Http/Controllers/PersonController.php
+++ src/app/Http/Controllers/PersonController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Person;
 
 class PersonController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $items = Person::all();
+        return view('person.index', ['items' => $items]);
+    }
 }
